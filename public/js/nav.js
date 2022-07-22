d564c174b0da28614f950fc2af0da8b26799ad46
Rushmitha S <rushmitha6@gmail.com>
2022-07-22 23:35:36 +0530

    first commit

---
+++ public/js/nav.js
@@ -0,0 +1,29 @@
+const createNav =() =>
+{
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML= `
+    <div class="nav">
+    <img class="brand-logo" src="img//dark-logo.png">
+    <div class="nav-items">
+        <div class="search">
+           <input type="text" class="search-box" placeholder="search brand,product">
+           <button type="button" class="search-btn">search</button>
+       </div>
+   <a href="#"><img src="img/user.png" alt="userimage"></a>     
+   <a href="#"><img src="img/cart.png" alt="cardimage"></a>    
+</div>
+
+</div>
+<ul class="links-container">
+<li class="link-item"><a href="#" class="link">home</a></li>
+<li class="link-item"><a href="#" class="link">women</a></li>
+<li class="link-item"><a href="#" class="link">men</a></li>
+<li class="link-item"><a href="#" class="link">kids</a></li>
+<li class="link-item"><a href="#" class="link">accessories</a></li>
+
+</ul>
+    `;
+}
+
+createNav();
